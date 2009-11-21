5d68562313631f551aa88e1c4cc88e19444f85f3
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-21 16:09:33 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "juragan",
+    version = "1.0",
+    url = 'http://www.ubuntu-id.org/',
+    license = 'BSD',
+    description = 'Distribusi DVD Repository Ubuntu',
+    author = 'Fajran Iman Rusadi',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = ['setuptools', 'django-registration']
+)
+
