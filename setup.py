c2ad25039056c41ecf2af1151b0aa078172c40f3
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-21 18:51:23 +0100

    Tambahkan simplejson

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author = 'Fajran Iman Rusadi',
     packages = find_packages('src'),
     package_dir = {'': 'src'},
-    install_requires = ['setuptools', 'django-registration']
+    install_requires = ['setuptools', 'django-registration', 'simplejson']
 )
 
