e03219b0c32f19fbfca4e93d0cd06277b4c65ad0
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-20 11:53:29 +0100

    Halaman bantuan

--- src/juragan/urls.py
+++ src/juragan/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,5 +16,7 @@ urlpatterns = patterns('',
     (r'^lokasi/(?P<posisi>.+)/$', 'juragan.views.lokasi'),
     (r'^toko/daftar/$', 'juragan.views.daftar'),
     (r'^toko/(?P<toko_id>\d+)/$', 'juragan.views.toko'),
+    (r'^bantuan/$', direct_to_template, 
+        {'template': 'bantuan.html'}),
     (r'^$', 'juragan.views.index'),
 )
